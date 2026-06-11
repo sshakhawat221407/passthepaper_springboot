6d7af37a3d048616747dcf21f79ada69d830bbf0
Samsad Ibne Shakhawat (Nuhash) <sshakhawat221407@bscse.uiu.ac.bd>
2026-06-11 20:16:26 +0600

    Add delete methods for User and Resource

--- src/main/java/com/passthepaper/repository/PurchaseRepository.java
+++ src/main/java/com/passthepaper/repository/PurchaseRepository.java
@@ -1,14 +1,12 @@
 package com.passthepaper.repository;
 
 import com.passthepaper.entity.*;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -22,4 +20,10 @@ public interface PurchaseRepository extends JpaRepository<Purchase, UUID> {
 
     @Query("SELECT COUNT(p) FROM Purchase p WHERE p.resource.uploadedBy = :uploader AND p.rating IS NOT NULL")
     Long countRatingsByUploader(@Param("uploader") User uploader);
+
+    @Transactional
+    void deleteByUser(User user);
+
+    @Transactional
+    void deleteByResource(Resource resource);
 }
