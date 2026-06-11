52e05adcb44a00e42bd915652daaeed1802c4d45
Samsad Ibne Shakhawat (Nuhash) <sshakhawat221407@bscse.uiu.ac.bd>
2026-06-11 20:15:37 +0600

    Add deleteByUser method to WithdrawalRepository

--- src/main/java/com/passthepaper/repository/WithdrawalRepository.java
+++ src/main/java/com/passthepaper/repository/WithdrawalRepository.java
@@ -1,18 +1,18 @@
 package com.passthepaper.repository;
 
 import com.passthepaper.entity.*;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface WithdrawalRepository extends JpaRepository<Withdrawal, UUID> {
     List<Withdrawal> findByUserOrderByCreatedAtDesc(User user);
     List<Withdrawal> findByStatusOrderByCreatedAtDesc(Withdrawal.WithdrawalStatus status);
+
+    @Transactional
+    void deleteByUser(User user);
 }
