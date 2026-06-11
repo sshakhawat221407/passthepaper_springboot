41ca03ea9ba473b3c1c1f27239f86a1bde0cb0f9
Samsad Ibne Shakhawat (Nuhash) <sshakhawat221407@bscse.uiu.ac.bd>
2026-06-11 20:14:00 +0600

    Add deleteByUser method to AppealRepository

--- src/main/java/com/passthepaper/repository/AppealRepository.java
+++ src/main/java/com/passthepaper/repository/AppealRepository.java
@@ -1,18 +1,18 @@
 package com.passthepaper.repository;
 
 import com.passthepaper.entity.*;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AppealRepository extends JpaRepository<Appeal, UUID> {
     List<Appeal> findByUserOrderByCreatedAtDesc(User user);
     List<Appeal> findByStatusOrderByCreatedAtDesc(Appeal.AppealStatus status);
+
+    @Transactional
+    void deleteByUser(User user);
 }
